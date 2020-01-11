234db15426c59fd8f5276044f3b5106be97a9562
Gautam Mundewadi <gautammundewadi@gmail.com>
2020-01-10 21:00:45 -0800

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <h1>Hello Gaucho Class!</h1>;
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// https://github.com/facebook/create-react-app
+
+ReactDOM.render(<App />, document.getElementById("root"));
