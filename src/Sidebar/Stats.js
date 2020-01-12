853a9f0805227ea8eac0a9da2c439b7ef1a15d9e
Tobias Fischer <fischer.m.tobias@gmail.com>
2020-01-12 03:33:19 -0800

    More visual edits

--- src/Sidebar/Stats.js
+++ src/Sidebar/Stats.js
@@ -17,16 +17,16 @@ class Stats extends React.Component {
 
     render() {
         return (
-            <div style={{ color: "white", zIndex: 0, textAlign: "center", width: "100%", position: "block" }}>
-                <div style={{ float: "left", margin: 23, marginBottom: 5, fontSize: 24 }}>
+            <div style={{ color: "white", zIndex: 0, textAlign: "center", width: "100%", position: "block", face: "bold" }}>
+                <div style={{ float: "left", margin: 23, marginBottom: 5, fontSize: 32 }}>
                     <div id="courseCount" style={{ textAlign: "center" }}>{this.props.courses.length}</div>
                     <div style={{ textAlign: "center", fontSize: 12 }}>COURSES</div>
                 </div>
                 <div style={{ float: "left", margin: 23, marginBottom: 5 }}>
-                    <div id="unitCount" style={{textAlign: "center", fontSize: 24}}>{this.calculateUnits(this.props.courses)}</div>
+                    <div id="unitCount" style={{textAlign: "center", fontSize: 32}}>{this.calculateUnits(this.props.courses)}</div>
                     <div style={{ textAlign: "center", fontSize: 12 }}>UNITS</div>
                 </div>
-                <div style={{ float: "left", margin: 23, marginBottom: 5, fontSize: 24 }}>
+                <div style={{ float: "left", margin: 23, marginBottom: 5, fontSize: 32, }}>
                     <div id="courseCount" style={{ textAlign: "center" }}>{3 * this.calculateUnits(this.props.courses)}</div>
                     <div style={{ textAlign: "center", fontSize: 12 }}>HRS/WEEK</div>
                 </div>
